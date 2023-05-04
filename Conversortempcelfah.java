bae1d5fb71a07067a606799060ab1b774d2f7123
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-05-04 11:58:13 -0300

    commit Samuel e Pedro Santos

---
+++ Conversortempcelfah.java
@@ -0,0 +1,33 @@
+
+public class Conversortempcelfah {
+	public static void main(String[] args) {
+		//Declaração de variáveis
+		double fah, cel;
+		
+		//Criação e instância do objeto de entrada
+		Scanner conta = new Scanner(System.in);
+		
+		//Apresentação
+		System.out.print("\n\t\t\t --- Calculadora Celsius para Fahreinheint --- \n");
+		
+		//Entrada
+		System.out.print("Informe Cel: ");
+		cel = conta.nextDouble();
+		
+		//Processamento
+		fah = 9*cel/5 + 32;
+				
+		//Saída
+		System.out.println("\n\n" + "Cel: " + cel + " = " + "Fah: " + fah);
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
