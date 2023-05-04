43157b736acea31bba8b2bb8107d3ce41a57f473
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-05-04 14:00:55 -0300

    Commit Samuel e Pedro Santos

--- Conversortempcelfah.java
+++ Conversortempcelfah.java
@@ -15,30 +15,37 @@ public class Conversortempcelfah {
             System.out.println("1. Cel para Fah");
             System.out.println("2. Fah to Cel");
             System.out.println("3. Sair");
+
+            //Opção do usuário
+            System.out.print(" Opção: ");
+            op = entrada.nextDouble();
+
+
+
         if(op == 1){
-                System.out.println("\n\t\t\t -- Soma --\n");
+                System.out.println("\n\t\t\t -- Celsius para Fahrenheit --\n");
 
                 //Entrada
-                System.out.println("Informe Cel: ");
+                System.out.println("Informe Celsius: ");
                 cel = entrada.nextDouble();
 
                 //Processamento
                 fah = 9*cel/5 + 32;
 
                 //Saida
-                System.out.printf("%d = %d", cel, fah);
+                System.out.println("\n\n" + "Cel: " + cel + "°C" + " = " + "Fah: " + fah + "°F" );
             }else if (op == 2){
-                System.out.println("\n\t\t\t -- Subtração --\n");
+                System.out.println("\n\t\t\t -- Fahrenheit --\n");
 
                 //Entrada
-                System.out.println("Informe Fah: ");
+                System.out.println("Informe Fahrenheit: ");
                 fah = entrada.nextDouble();
 
                 //Processamento
-                fah = 9*cel/5 + 32;
+                cel = (fah - 32)*5/9;
 
                 //Saida
-                System.out.printf("%d = %d", fah, cel);
+                System.out.println("\n\n" + "Fah: " + fah + "°F" + " = " + "Cel: " + cel + "°C" );
             }else if (op == 3){
                 System.out.println("Forte abraço!");
             }else{
@@ -46,6 +53,6 @@ public class Conversortempcelfah {
             }
             
 
-        }
+        }while(op!=3);
 }
 }
